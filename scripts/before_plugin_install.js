2e1ddcfedd17b31b2f8c65dab558d7e928a8dd57
Arun Yokesh Kumar <arun@ampcome.com>
2019-10-28 19:40:28 +0530

    Modified  hook script for proper injection

--- scripts/before_plugin_install.js
+++ scripts/before_plugin_install.js
@@ -6,10 +6,11 @@ const path = require('path')
 module.exports = function (context) {
     console.log("before_plugin_install script is called");
     return new Promise((resolve, reject) => {
+        console.log("context", JSON.stringify(context));
         var rootPath = context.opts.projectRoot;
         var configXmlPath = path.join(rootPath, 'config.xml');
         var configXml = fs.readFileSync(configXmlPath, "utf-8");
-        var pluginXml = fs.readFileSync('plugin.xml', "utf-8");
+        var pluginXml = fs.readFileSync(context.opts.plugin.dir + "/plugin.xml", "utf-8");
         console.log("pluginXml", pluginXml, "configXml", configXml);
         // we then pass the data to our method here
         parseString(configXml, function (err, configXmlParsed) {
@@ -18,7 +19,7 @@ module.exports = function (context) {
                 reject(err);
             } else {
                 // here we log the results of our xml string conversion
-                console.log(configXmlParsed);
+                //console.log(configXmlParsed);
                 console.dir(configXmlParsed.widget.$.id);
 
                 parseString(pluginXml, function (err, pluginXmlParsed) {
@@ -31,18 +32,20 @@ module.exports = function (context) {
                         var androidPlatform = pluginXmlParsed.plugin.platform.find(function (p) { return p.$.name == "android"; })
                         var cf = androidPlatform["config-file"].find(function (cf) { return cf.provider && cf.provider.length > 0; });
                         var p = cf.provider.find(function (p) { return p.$["android:name"] && p.$["android:name"] == "com.ezetap.sdk.EzetapFileProvider" })
-                        p.$["android:authorities"] = configXmlParsed.widget.$.id + ".EzetapFileProvider";
-                        var builder = new xml2js.Builder();
-                        var xml = builder.buildObject(pluginXmlParsed);
-                        fs.writeFile(pluginXml, xml, function (err, data) {
-                            if (err) {
-                                console.log(err);
-                                reject(err)
-                            } else {
-                                console.log("successfully written our update xml to file");
-                                resolve('done');
-                            }
-                        });
+                        if (p) {
+                            p.$["android:authorities"] = configXmlParsed.widget.$.id + ".EzetapFileProvider";
+                            var builder = new xml2js.Builder();
+                            var xml = builder.buildObject(pluginXmlParsed);
+                            fs.writeFile(context.opts.plugin.dir + "/plugin.xml", xml, function (err, data) {
+                                if (err) {
+                                    console.log(err);
+                                    reject(err)
+                                } else {
+                                    console.log("successfully written our update xml to file");
+                                    resolve('done');
+                                }
+                            });
+                        }
                     }
                 });
             }
